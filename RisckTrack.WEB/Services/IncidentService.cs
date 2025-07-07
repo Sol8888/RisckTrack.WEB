077dd5cd9b0713172663480fc4f2ce81e4b1abb7
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-07-07 09:47:44 -0500

    Create Service for Incident

---
+++ RisckTrack.WEB/Services/IncidentService.cs
@@ -0,0 +1,31 @@
+﻿using RisckTrack.WEB.Models;
+
+namespace RisckTrack.WEB.Services
+{
+    public class IncidentService
+    {
+        private readonly HttpClient _http;
+
+        public IncidentService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<IncidentDto>?> GetIncidentsAsync()
+        {
+            return await _http.GetFromJsonAsync<List<IncidentDto>>("api/Incidents");
+        }
+
+        public async Task<bool> CreateIncidentAsync(IncidentDto dto)
+        {
+            var response = await _http.PostAsJsonAsync("api/Incidents", dto);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteIncidentAsync(int id)
+        {
+            var response = await _http.DeleteAsync($"api/Incidents/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
