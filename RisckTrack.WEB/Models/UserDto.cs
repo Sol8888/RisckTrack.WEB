fe0c0313ab2d7e6fabe230b2deb0ddfd654ec07d
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-07-06 19:35:33 -0500

    Create Company and User models

---
+++ RisckTrack.WEB/Models/UserDto.cs
@@ -0,0 +1,11 @@
+﻿namespace RisckTrack.WEB.Models
+{
+    public class UserDto
+    {
+        public int? CompanyId { get; set; }
+        public string? Username { get; set; }
+        public string? Email { get; set; }
+        public string? Password { get; set; }
+        public string Role { get; set; } = "U";
+    }
+}
