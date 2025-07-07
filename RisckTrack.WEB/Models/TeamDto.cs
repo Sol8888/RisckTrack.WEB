dbb3ee681d89cb28192f38f42c04a7585080dccf
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-07-07 10:22:59 -0500

    Create Model for Team

---
+++ RisckTrack.WEB/Models/TeamDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RisckTrack.WEB.Models
+{
+    public class TeamDto
+    {
+        public int CompanyId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Leader { get; set; } = string.Empty;
+        public string ContactEmail { get; set; } = string.Empty;
+    }
+}
