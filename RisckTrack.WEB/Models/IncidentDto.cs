a335df1b16b2cc305bfc32b1f266153d4c9f7d09
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-07-07 09:46:08 -0500

    Create model for Incident

---
+++ RisckTrack.WEB/Models/IncidentDto.cs
@@ -0,0 +1,12 @@
+﻿namespace RisckTrack.WEB.Models
+{
+    public class IncidentDto
+    {
+        public int IncidentId { get; set; }
+        public string AssetId { get; set; } = string.Empty;
+        public DateTime IncidentDate { get; set; } = DateTime.Now;
+        public string Description { get; set; } = string.Empty;
+        public int? ResolutionDurationHours { get; set; }
+        public int? ImpactDurationMinutes { get; set; }
+    }
+}
