4c5a2eaec88fd1081bc49032366e33147064821f
Sol8888 <daniela.cabrera.tapia@udla.edu.ec>
2025-07-07 10:23:17 -0500

    Create Service for Teams

---
+++ RisckTrack.WEB/Services/TeamService.cs
@@ -0,0 +1,31 @@
+﻿using RisckTrack.WEB.Models;
+
+namespace RisckTrack.WEB.Services
+{
+    public class TeamService
+    {
+        private readonly HttpClient _http;
+
+        public TeamService(HttpClient http)
+        {
+            _http = http;
+        }
+
+        public async Task<List<TeamDto>?> GetTeamsAsync()
+        {
+            return await _http.GetFromJsonAsync<List<TeamDto>>("api/Teams");
+        }
+
+        public async Task<bool> CreateTeamAsync(TeamDto dto)
+        {
+            var response = await _http.PostAsJsonAsync("api/Teams", dto);
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> DeleteTeamAsync(int id)
+        {
+            var response = await _http.DeleteAsync($"api/Teams/{id}");
+            return response.IsSuccessStatusCode;
+        }
+    }
+}
