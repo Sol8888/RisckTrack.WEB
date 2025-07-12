8c3459da9048d72a730002f8a8f58e7f55d68171
Kpiiale <yuliana.capito@udla.edu.ec>
2025-07-12 04:10:06 -0500

    Add new model for 2FA

---
+++ RisckTrack.WEB/Models/User2FAVerifyDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace RisckTrack.WEB.Models
+{
+    public class User2FAVerifyDto
+    {
+
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "El campo codigo es obligatorio.")]
+        [JsonPropertyName("Code")]
+        public string Code { get; set; }
+
+    }
+}
